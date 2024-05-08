43c6f4fd9ea78383055019e731a7186b3d70e13c
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2024-05-08 19:30:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,21 @@ import streamlit.components.v1 as components
 
 
 
-
-
-# Define CSS to hide GitHub icon and maximize iframe height
-css_code = """
+# Define CSS to hide GitHub icon
+hide_github_icon = """
 <style>
 #GithubIcon { 
     visibility: hidden;
 }
-iframe {
-    height: 100vh; /* 100% of the viewport height */
-    width: 100%; /* 100% of the available width */
-}
 </style>
 """
 
-# Define the iframe code
+# Define the iframe code with height set to 100vh
 iframe_code = """
-<iframe allow="web-share; fullscreen" style="border:none; width: 100% !important; position: static;display: block !important; margin: 0 !important;" src="https://amuselabs.com/pmm/crossword?id=109e269b&set=38549d5e35e37b3e5de58138b84c6564ac41d6e667cc86e5733632cc3c3197f8&embed=1" aria-label="Puzzle Me Game"></iframe>
+<iframe height="100vh" width="500%" allow="web-share; fullscreen" style="border:none; width: 100% !important; position: static;display: block !important; margin: 0 !important;" src="https://amuselabs.com/pmm/crossword?id=109e269b&set=38549d5e35e37b3e5de58138b84c6564ac41d6e667cc86e5733632cc3c3197f8&embed=1" aria-label="Puzzle Me Game"></iframe>
 """
 
 # Combine CSS and iframe code and display using components.html()
-components.html(css_code + iframe_code)
+components.html(hide_github_icon + iframe_code)
+
 
