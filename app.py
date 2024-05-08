4d41df058fe59907ea7273a965a7503ad785aa5f
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2024-05-08 19:13:58 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+#import streamlit as st
+
+# Hide GitHub icon CSS
+hide_github_icon = """
+<style>
+#GithubIcon { 
+    visibility: hidden;
+}
+</style>
+"""
+
+
+iframe_code = """
+<iframe height="700px" width="100%" allow="web-share; fullscreen" style="border:none; width: 100% !important; position: static;display: block !important; margin: 0 !important;" src="https://amuselabs.com/pmm/crossword?id=109e269b&set=38549d5e35e37b3e5de58138b84c6564ac41d6e667cc86e5733632cc3c3197f8&embed=1" aria-label="Puzzle Me Game"></iframe>
+"""
+
+st.components.v1.html(iframe_code)
