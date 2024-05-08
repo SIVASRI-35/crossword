a8736d3142fc6ea1d3f77acfc2b602aad6ca27ff
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2024-05-08 19:32:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ hide_github_icon = """
 
 # Define the iframe code with height set to 100vh
 iframe_code = """
-<iframe height="100vh" width="500%" allow="web-share; fullscreen" style="border:none; width: 100% !important; position: static;display: block !important; !important;" src="https://amuselabs.com/pmm/crossword?id=109e269b&set=38549d5e35e37b3e5de58138b84c6564ac41d6e667cc86e5733632cc3c3197f8&embed=1" aria-label="Puzzle Me Game"></iframe>
+<iframe height="100vh" width="500%" allow="web-share; fullscreen" style="border:none; width: 100% ; !important; !important;" src="https://amuselabs.com/pmm/crossword?id=109e269b&set=38549d5e35e37b3e5de58138b84c6564ac41d6e667cc86e5733632cc3c3197f8&embed=1" aria-label="Puzzle Me Game"></iframe>
 """
 
 # Combine CSS and iframe code and display using components.html()
