a6db847c3056dc8ab044996a0a0e458b53ab1f87
S.Siva Sri Varshini <115179509+SIVASRI-35@users.noreply.github.com>
2024-05-08 19:25:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ hide_github_icon = """
 
 
 iframe_code = """
-<iframe height="2000px" width="100%" allow="web-share; fullscreen" style="border:none; width: 100% !important; position: static;display: block !important; margin: 0 !important;" src="https://amuselabs.com/pmm/crossword?id=109e269b&set=38549d5e35e37b3e5de58138b84c6564ac41d6e667cc86e5733632cc3c3197f8&embed=1" aria-label="Puzzle Me Game"></iframe>
+<iframe height="5000px" width="500%" allow="web-share; fullscreen" style="border:none; width: 100% !important; position: static;display: block !important; margin: 0 !important;" src="https://amuselabs.com/pmm/crossword?id=109e269b&set=38549d5e35e37b3e5de58138b84c6564ac41d6e667cc86e5733632cc3c3197f8&embed=1" aria-label="Puzzle Me Game"></iframe>
 """
 
 components.html(iframe_code)
